5ea167d2ef50bee6b2e587c64856659cbd3762c6
Peng Chang <changpengshuai@gmail.com>
2021-05-26 21:39:07 -0400

    change from subscribe once to infinite

--- lfd/gen3_joints_trajectory.py
+++ lfd/gen3_joints_trajectory.py
@@ -5,46 +5,45 @@ from kortex_driver.msg import JointAngle
 from trajectory_msgs.msg import JointTrajectory
 import copy
 
-rospy.init_node('haha', anonymous=True)
-client = rospy.ServiceProxy("/my_gen3/base/play_joint_trajectory", PlayJointTrajectory)
 
-request = PlayJointTrajectoryRequest()
+def callback(msg):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
 
-joint_position = [JointAngle() for i in range(7)]
-# joint_position = JointAngles[]
-for i, j in enumerate(joint_position):
-    j.joint_identifier = i + 1
+    client = rospy.ServiceProxy("/my_gen3/base/play_joint_trajectory", PlayJointTrajectory)
 
-msg = rospy.wait_for_message("/planned_trajectory", JointTrajectory)
+    request = PlayJointTrajectoryRequest()
 
-for i in range(7): 
-    if msg.points[0].positions[i]<0 :
-        joint_position[i].value = msg.points[0].positions[i]*180.0/3.141592653 + 360
+    joint_position = [JointAngle() for i in range(7)]
+    # joint_position = JointAngles[]
+    for i, j in enumerate(joint_position):
+        j.joint_identifier = i + 1
+
+    # msg = rospy.wait_for_message("/planned_trajectory", JointTrajectory)
+
+    for i in range(7): 
+        if msg.points[0].positions[i]<0 :
+            joint_position[i].value = msg.points[0].positions[i]*180.0/3.141592653 + 360
+        
+        else :
+            joint_position[i].value = msg.points[0].positions[i]*180.0/3.141592653
+        
+
+        print(joint_position[i])
+
+    request.input.joint_angles.joint_angles = joint_position
+
+    result = client(request)
+    print(result)
+
     
-    else :
-        joint_position[i].value = msg.points[0].positions[i]*180.0/3.141592653
-    
+def listener():
 
-    print(joint_position[i])
+    rospy.init_node('listener', anonymous=True)
 
-# extra = JointAngle()
-# extra.joint_identifier = 1
-# extra.value = 354.04
-# joint_position.append(extra)
+    rospy.Subscriber("/planned_trajectory", JointTrajectory, callback)
 
-# jp2 = copy.deepcopy(joint_position)
-# jp2[0].value = 354.04
-# jp2[1].value = 9.88
-# jp2[2].value = 165.87
-# jp2[3].value = 277.08
-# jp2[4].value = 4.28
-# jp2[5].value = 276.02
-# jp2[6].value = 61.73
+    rospy.spin()
 
-# print(type(request))
-request.input.joint_angles.joint_angles = joint_position
+if __name__ == '__main__':
 
-
-result = client(request)
-print(result)
-rospy.spin()
+    listener()
